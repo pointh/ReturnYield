be3ab7b404b60ad09e563a130b09a6c006fc0228
Pavel Svec <pavel.svec@itcrawl.cz>
2022-03-01 11:46:02 +0100

    Add project files.

---
+++ ReturnYield/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReturnYield
+{
+    class Program
+    {
+        private static IEnumerable<long> Primes()
+        {
+            long candidate = 2;
+
+            while (true)
+            {
+                int i = 2;
+                while(i * i <= candidate)
+                {
+                    if (candidate % i++ == 0)
+                    {
+                        candidate++;
+                        i = 2;
+                        continue;
+                    }
+
+                }
+                yield return candidate;
+                candidate++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            foreach(var x in Primes())
+            {
+                string s = x.ToString();
+                if(s[s.Length-1] == '1')
+                {
+                        Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
